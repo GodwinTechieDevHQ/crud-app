8b62e1354c243353d4f4525273ad6922f93721cb
Godwin John <gj09042003@gmail.com>
2025-03-24 04:13:59 +0100

    Finalized the journal entry context

--- anchor/programs/crud-app/src/lib.rs
+++ anchor/programs/crud-app/src/lib.rs
@@ -8,19 +8,28 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod crudapp {
     use super::*;
 
-    pub fn create_journal_entry(ctx: Context<CreateEntry>, ) -> Result<()> {
+    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String) -> Result<()> {
 
     }
  
 }
 
 #[derive(accounts)]
+#[instruction(title: String)]
 pub struct CreateEntry<'info> {
   #[account(
     init,
     seeds = [title.as_bytes(), owner.key.as_ref()],
     bump,
+    space = 8 + JournalEntryState::INIT_SPACE;
+    payer = owner;
   )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>,
 }
 
 #[account]
